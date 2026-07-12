b9ff4e3e84a2dd272eeb5e96fb5c6fe8fba2b090
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-12 22:36:38 +0100

    Create bash.sh

---
+++ arc-agi-3/bash.sh
@@ -0,0 +1,8 @@
+cd simulations
+
+# Attack simulations
+python attack_simulations.py               # Core attack simulation framework
+python attack_track_fb.py                  # Trust manipulation attacks
+python attack_track_fc.py                  # Economic attacks
+
+# For full 4-Life game demos, see: https://github.com/dp-web4/4-life
